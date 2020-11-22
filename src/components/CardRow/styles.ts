af8998267f32ff2221fa418c7320681bc41f38bc
Thiago Franchin <thiago.franchin@gmail.com>
2020-11-22 18:59:21 -0300

    Card Row

--- src/components/CardRow/styles.ts
+++ src/components/CardRow/styles.ts
@@ -3,16 +3,35 @@ import styled from 'styled-components'
 export const CardRowHeader = styled.div`
   padding-bottom: 50px;
 
-  p {
-    padding: 0 200px;
+  @media (min-width: 768px) {
+    p {
+      padding: 0 100px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    p {
+      padding: 0 200px;
+    }
   }
 `
 
-export const CardRowColumnWrapper = styled.div``
+export const CardRowColumnWrapper = styled.div`
+  @media (max-width: 767.98px) {
+    margin-bottom: 55px;
+  }
+`
 export const CardRowTitle = styled.h3`
   text-align: center;
-  margin-bottom: 50px;
 `
+
+export const Divider = styled.div`
+  border-top: 1px solid #222222;
+  height: 0px;
+  margin: 35px auto;
+  width: 100px;
+`
+
 export const CardRowParagraph = styled.p`
   font-size: 15px;
   font-weight: 400;
