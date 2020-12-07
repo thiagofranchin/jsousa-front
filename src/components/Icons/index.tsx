608195b0ea7b4d6b8ecf65e97617be43fc541f2f
Thiago Franchin <thiago.franchin@gmail.com>
2020-12-07 02:42:57 -0300

    Carousel and Hero in single component, add icon angle

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -2,6 +2,8 @@ import * as S from './styles'
 import { FacebookSquare } from '@styled-icons/boxicons-logos/FacebookSquare'
 import { Instagram } from '@styled-icons/boxicons-logos/Instagram'
 import { Phone } from '@styled-icons/boxicons-regular/Phone'
+import { AngleLeft } from '@styled-icons/fa-solid/AngleLeft'
+import { AngleRight } from '@styled-icons/fa-solid/AngleRight'
 
 type Props = {
   colorIcon?: string
@@ -12,6 +14,8 @@ const Icons = ({ colorIcon = '#28B0D1' }: Props) => (
     <FacebookSquare width="50" color={colorIcon} />
     <Instagram width="50" color={colorIcon} />
     <Phone width="50" color={colorIcon} />
+    <AngleLeft width="25" color={colorIcon} />
+    <AngleRight width="25" color={colorIcon} />
   </S.Wrapper>
 )
 
